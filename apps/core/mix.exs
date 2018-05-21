64ec68b133300db257ee57808bee31a0915f48b8
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-05-21 17:14:47 +0300

    Add email, redis and jsonrpc dependencies

--- apps/core/mix.exs
+++ apps/core/mix.exs
@@ -9,7 +9,7 @@ defmodule Core.Mixfile do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -36,10 +36,15 @@ defmodule Core.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:distillery, "~> 1.5", runtime: false},
+      {:quorum, in_umbrella: true},
       {:phoenix, "~> 1.3.2"},
       {:cowboy, "~> 1.1"},
-      {:quorum, in_umbrella: true}
+      {:swoosh, "~> 0.14"},
+      {:redix, ">= 0.0.0"},
+      {:jsonrpc2, "~> 1.0"},
+      {:shackle, "~> 0.5"}, # tcp client for jsonrps2
+
+      {:distillery, "~> 1.5", runtime: false}
     ]
   end
 end
