99fcac8a61104916076c45229414f5e3c18b3d99
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-05-22 14:23:00 +0300

    Update redis client

--- apps/core/lib/core/foundation/redis/redis.ex
+++ apps/core/lib/core/foundation/redis/redis.ex
@@ -1,31 +1,41 @@
-defmodule Core.Foundation.Clients.Redis do
+defmodule Core.Foundation.Redis do
   @moduledoc false
 
-  @behaviour Core.Foundation.Clients.Redis.Behaviour
-
   @spec get(binary) :: {:ok, term} | {:error, binary}
-  def get(key) do
+  def get(key) when is_binary(key) do
     with {:ok, encoded_value} <- Redix.command(:redix, ["GET", key]) do
-      decode(encoded_value)
+      {:ok, decode(encoded_value)}
     end
   end
 
   @spec set(binary, term, pos_integer | nil) :: {:ok, term} | {:error, binary}
   def set(key, value, ttl \\ nil)
-  def set(key, value, nil) when value != nil, do: do_set(["SET", key, encode(value)])
-  def set(key, value, ttl) when value != nil, do: do_set(["SET", key, encode(value), "EX", ttl])
+
+  def set(key, value, nil) when is_binary(key) and value != nil,
+    do: do_set(["SET", key, encode(value)])
+
+  def set(key, value, ttl) when is_binary(key) and value != nil,
+    do: do_set(["SET", key, encode(value), "EX", ttl])
 
   @spec do_set(list) :: :ok | {:error, binary}
   defp do_set(params) do
     case Redix.command(:redix, params) do
       {:ok, _} -> :ok
-      error -> error
+      err -> err
     end
   end
 
-  @spec del(binary) :: {:ok, term} | {:error, binary}
-  def del(key) do
+  @spec delete(binary) :: {:ok, term} | {:error, binary}
+  def delete(key) when is_binary(key) do
     Redix.command(:redix, ["DEL", key])
+  end
+
+  @spec flush :: :ok | {:error, binary}
+  def flush do
+    case Redix.command(:redix, ["FLUSHDB"]) do
+      {:ok, _} -> :ok
+      err -> err
+    end
   end
 
   @spec encode(term) :: term
