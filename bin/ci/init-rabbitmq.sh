884b6ba16a02f7cabc6d0ae712179e240c861a82
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-05-28 19:45:33 +0300

    create rabbit-mq user

--- bin/ci/init-rabbitmq.sh
+++ bin/ci/init-rabbitmq.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 echo "NODE_IP_ADDRESS=" >> /etc/rabbitmq//main/pg_hba.conf
-
-service rabbitmqctl add_user test test
-service rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
-
-service rabbitmqctl stop
-service rabbitmq-server
