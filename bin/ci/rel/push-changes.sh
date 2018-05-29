369706767f6a90b106848583c3854f7b5e6f7daf
PavelVesnin <pavel.vesnin.v@gmail.com>
2018-05-29 13:14:36 +0300

    [patch] github push changes #6

--- bin/ci/rel/push-changes.sh
+++ bin/ci/rel/push-changes.sh
@@ -38,8 +38,8 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
     ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
     ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
-    chmod 600 deploy_key
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in "${PROJECT_DIR}/../../github_deploy_key.enc" -out github_deploy_key -d
+    chmod 600 github_deploy_key
     eval `ssh-agent -s`
     ssh-add deploy_key
 
