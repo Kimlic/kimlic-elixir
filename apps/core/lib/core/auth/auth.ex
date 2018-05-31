aac4054962315f9dc332b2067b7bb2038a4a4252
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-05-31 13:39:33 +0300

    [patch] Generate 6 number code instead of long string

--- apps/core/lib/core/auth/auth.ex
+++ apps/core/lib/core/auth/auth.ex
@@ -6,7 +6,7 @@ defmodule Core.Auth do
 
   @spec create_profile(binary, binary) :: :ok | {:error, binary}
   def create_profile(email, account_address) do
-    with {:ok, verification} <- Verifications.create_email_verification(email, account_address) do
+    with {:ok, verification} <- Verifications.create_email_verification(account_address) do
       Email.send_verification(email, verification)
     end
   end
--- apps/core/lib/core/verifications/token_generator.ex
+++ apps/core/lib/core/verifications/token_generator.ex
@@ -2,14 +2,9 @@ defmodule Core.Verifications.TokenGenerator do
   @moduledoc false
 
   alias Core.Verifications.TokenGeneratorBehaviour
-  alias Ecto.UUID
 
   @behaviour TokenGeneratorBehaviour
 
-  @spec generate_email_token(binary, binary) :: binary
-  def generate_email_token(email, account_address) do
-    :sha256
-    |> :crypto.hash(Enum.join([UUID.generate(), email, account_address]))
-    |> Base.encode16()
-  end
+  @spec generate_code :: binary
+  def generate_code, do: "#{Enum.random(1_000_000..9_999_999)}"
 end
