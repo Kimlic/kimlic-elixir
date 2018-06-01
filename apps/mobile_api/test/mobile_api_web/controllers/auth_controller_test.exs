417dab275cdacc3e0bc12a2d97a0811d1b30d751
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-06-01 17:53:33 +0300

    [minor] Separate token generation for email and phone

--- apps/mobile_api/test/mobile_api_web/controllers/auth_controller_test.exs
+++ apps/mobile_api/test/mobile_api_web/controllers/auth_controller_test.exs
@@ -10,31 +10,30 @@ defmodule MobileApi.AuthTest do
   alias Core.Factory
   alias Core.StorageKeys
   alias Core.Verifications.Verification
+  alias Core.Verifications.TokenGenerator
 
   @entity_type_email Verification.entity_type(:email)
   @entity_type_phone Verification.entity_type(:phone)
 
   describe "create profile test" do
     test "success", %{conn: conn} do
-      for _ <- 0..100 do
-        account_address = generate_account_address()
-        email = "test#{generate_token()}@email.com"
-        token = generate_token()
+      account_address = generate_account_address()
+      token = TokenGenerator.generate(:email)
+      email = "test#{token}@email.com"
 
-        expect(TokenGeneratorMock, :generate_code, fn -> token end)
+      expect(TokenGeneratorMock, :generate, fn :email -> token end)
 
-        assert %{status: 201} =
-                 post(conn, auth_path(conn, :create_profile), data_for(:auth_create_profile, email, account_address))
+      assert %{status: 201} =
+               post(conn, auth_path(conn, :create_profile), data_for(:auth_create_profile, email, account_address))
 
-        assert {:ok, %Verification{account_address: ^account_address, entity_type: @entity_type_email}} =
-                 Redis.get(StorageKeys.vefirication_email(token))
-      end
+      assert {:ok, %Verification{account_address: ^account_address, entity_type: @entity_type_email}} =
+               Redis.get(StorageKeys.vefirication_email(token))
     end
   end
 
   describe "check verification token test" do
     test "success", %{conn: conn} do
-      token = generate_token()
+      token = TokenGenerator.generate(:email)
       verification = Factory.verification!(%{token: token})
       Redis.set(StorageKeys.vefirication_email(token), verification)
 
@@ -44,40 +43,36 @@ defmodule MobileApi.AuthTest do
     end
 
     test "not found", %{conn: conn} do
-      assert post(conn, auth_path(conn, :check_verification_token), %{"token" => generate_token()})
+      assert conn
+             |> post(auth_path(conn, :check_verification_token), %{"token" => TokenGenerator.generate(:email)})
              |> json_response(404)
     end
   end
 
   describe "create phone verification" do
     test "success", %{conn: conn} do
-      for _ <- 0..100 do
-        phone = generate_phone()
-        token = generate_token()
-        account_address = generate_account_address()
+      phone = generate_phone()
+      token = TokenGenerator.generate(:phone)
+      account_address = generate_account_address()
 
-        expect(TokenGeneratorMock, :generate_code, fn -> token end)
-        expect(MessengerMock, :send, fn ^phone, _message -> {:ok, %{}} end)
+      expect(TokenGeneratorMock, :generate, fn :phone -> token end)
+      expect(MessengerMock, :send, fn ^phone, _message -> {:ok, %{}} end)
 
-        assert %{status: 201} =
-                 post(
-                   conn,
-                   auth_path(conn, :create_phone_verification),
-                   data_for(:auth_create_phone_verification, account_address, phone)
-                 )
+      assert %{status: 201} =
+               post(
+                 conn,
+                 auth_path(conn, :create_phone_verification),
+                 data_for(:auth_create_phone_verification, account_address, phone)
+               )
 
-        assert {:ok, %Verification{account_address: ^account_address, entity_type: @entity_type_phone}} =
-                 Redis.get(StorageKeys.vefirication_phone(token))
-      end
+      assert {:ok, %Verification{account_address: ^account_address, entity_type: @entity_type_phone}} =
+               Redis.get(StorageKeys.vefirication_phone(token))
     end
   end
-
-  @spec generate_token :: binary
-  defp generate_token, do: Enum.random(100_000..999_999) |> to_string()
 
   @spec generate_phone :: binary
   defp generate_phone, do: "+38097#{Enum.random(1_000_000..9_999_999)}"
 
   @spec generate_account_address :: binary
-  defp generate_account_address, do: "0xf" <> (:md5 |> :crypto.hash(generate_token()) |> Base.encode16())
+  defp generate_account_address, do: "0xf" <> (:md5 |> :crypto.hash(TokenGenerator.generate(:email)) |> Base.encode16())
 end
