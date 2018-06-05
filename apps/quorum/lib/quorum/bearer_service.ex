0b11d8b594fe4c17a008fa931462b672887184b6
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-06-05 19:59:23 +0300

    [minor] Rename params in bearer service

--- apps/quorum/lib/quorum/bearer_service.ex
+++ apps/quorum/lib/quorum/bearer_service.ex
@@ -2,14 +2,14 @@ defmodule Quorum.BearerService do
   @moduledoc false
 
   @spec authorized?(binary, binary) :: boolean
-  def authorized?(request_bearer, auth_token), do: request_bearer == bearer(auth_token)
+  def authorized?(auth_token, key), do: auth_token == bearer(key)
 
   @spec bearer(binary) :: binary
-  def bearer(auth_token) do
+  def bearer(auth_key) do
     authorization_salt = Confex.fetch_env!(:quorum, :authorization_salt)
 
     :sha256
-    |> :crypto.hash(auth_token <> authorization_salt)
+    |> :crypto.hash(auth_key <> authorization_salt)
     |> Base.encode16()
   end
 end
