f79f9301bd02843f63dd99304cd6c9f9e0f342ff
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-06-29 17:40:11 +0300

    Remove kimlic ap credentials

--- apps/quorum/config/dev.exs
+++ apps/quorum/config/dev.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :quorum,
   authorization_salt: "1234567890",
-  kimlic_ap_address: "0xe60658b494ab6719cf46ad45a9b35ad3ac66aa3a",
+  kimlic_ap_address: "",
   kimlic_ap_password: "Kimlicp@ssw0rd",
-  context_storage_address: "0xf3bbc576c29ea3eee9ddda6ae44a4e381b5afcd0"
+  context_storage_address: ""
