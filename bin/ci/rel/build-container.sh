f433cad9730845acd54dcb4be323b26fc6efc377
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-07-31 17:44:18 +0300

    [patch] Add debug commands

--- bin/ci/rel/build-container.sh
+++ bin/ci/rel/build-container.sh
@@ -6,7 +6,7 @@ CONTAINER_VERSION="${PROJECT_VERSION}-${TRAVIS_JOB_NUMBER}"
 
 echo "[I] Building a Docker container '${PROJECT_NAME}' (version '${CONTAINER_VERSION}') from path '${PROJECT_DIR}'.."
 docker build --tag "${PROJECT_NAME}:${CONTAINER_VERSION}" \
-             --file "${PROJECT_DIR}/../../Dockerfile" \
+             --file "${PROJECT_DIR}/Dockerfile" \
              --build-arg APP_VERSION=$PROJECT_VERSION \
              --build-arg APP_NAME=$PROJECT_NAME \
              "$PROJECT_DIR/../../"
