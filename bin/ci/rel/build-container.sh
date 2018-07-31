cb51ada958afca2173e9c09afa01dd97fa4e264b
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-07-31 14:32:21 +0300

    [patch] Build docker container with right version

--- bin/ci/rel/build-container.sh
+++ bin/ci/rel/build-container.sh
@@ -2,9 +2,11 @@
 # This script builds an image based on a Dockerfile and mix.exs that is located in root of git working tree.
 set -e
 
-echo "[I] Building a Docker container '${PROJECT_NAME}' (version '${PROJECT_VERSION}') from path '${PROJECT_DIR}'.."
-docker build --tag "${PROJECT_NAME}:${PROJECT_VERSION}" \
+CONTAINER_VERSION="${PROJECT_VERSION}-${TRAVIS_JOB_NUMBER}"
+
+echo "[I] Building a Docker container '${PROJECT_NAME}' (version '${CONTAINER_VERSION}') from path '${PROJECT_DIR}'.."
+docker build --tag "${PROJECT_NAME}:${CONTAINER_VERSION}" \
              --file "${PROJECT_DIR}/Dockerfile" \
-             --build-arg APP_VERSION=$PROJECT_VERSION \
+             --build-arg APP_VERSION=$CONTAINER_VERSION \
              --build-arg APP_NAME=$PROJECT_NAME \
              "$PROJECT_DIR/../.."
