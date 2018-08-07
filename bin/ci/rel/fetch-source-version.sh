20d39e4d3b0ca343f91c0dddc6f5d2d7fe06768f
Denis Klimenko <denis.klimenko.dx@gmail.com>
2018-08-07 12:13:59 +0300

    [patch] Fetch source version from mix file instead of git history

--- bin/ci/rel/fetch-source-version.sh
+++ bin/ci/rel/fetch-source-version.sh
@@ -1,24 +1,9 @@
-# Get latest version
-PREVIOUS_VERSION=$(git describe HEAD --abbrev=0 --tags)
-if [[ "$?" != "0" ]]; then
-  PREVIOUS_VERSION="0.1.0"
-fi;
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+PREVIOUS_VERSION=$PROJECT_VERSION
 
-# Get release notes
-if [[ "${PREVIOUS_VERSION}" == "0.1.0" ]]; then
-  GIT_HISTORY=$(git log --no-merges --format="%B")
-else
-  GIT_HISTORY=$(git log --no-merges --format="%B" $PREVIOUS_VERSION..HEAD)
-fi;
-
-GIT_HISTORY_CLEANED=$(echo "${GIT_HISTORY}" | grep -v 'ci skip' | grep -v 'changelog skip' | sed 's/^* //g')
-GIT_HISTORY_CLEANED=$(echo -e "${GIT_HISTORY_CLEANED}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//') # Trim strings
-
-# Count tag occurrences
-MAJOR_CHANGES=$(grep -io '^\[major\]' <<< "${GIT_HISTORY_CLEANED}" | wc -l)
-MINOR_CHANGES=$(grep -io '^\[minor\]' <<< "${GIT_HISTORY_CLEANED}" | wc -l)
-PATCH_CHANGES=$(grep -io '^\[patch\]' <<< "${GIT_HISTORY_CLEANED}" | wc -l)
+LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+MAJOR_CHANGES=$(grep -io '^\[major\]' <<< "${LAST_COMMIT_MESSAGE}" | wc -l)
+MINOR_CHANGES=$(grep -io '^\[minor\]' <<< "${LAST_COMMIT_MESSAGE}" | wc -l)
+PATCH_CHANGES=$(grep -io '^\[patch\]' <<< "${LAST_COMMIT_MESSAGE}" | wc -l)
 
 # Convert values to numbers (trims leading spaces)
 MAJOR_CHANGES=$(expr $MAJOR_CHANGES + 0)
@@ -45,6 +30,11 @@ fi;
 
 NEXT_VERSION="${NEXT_MAJOR_VERSION}.${NEXT_MINOR_VERSION}.${NEXT_PATCH_VERSION}"
 
+if [[ "${MAJOR_CHANGES}" == "0" && "${MINOR_CHANGES}" == "0" && "${PATCH_CHANGES}" == "0" ]]; then
+  echo "[ERROR] No version changes was detected (no patch, minor or major changes)."
+  export VERSION_ERROR="[ERROR] No version changes was detected (no patch, minor or major changes)."
+fi;
+
 # Show version info
 echo
 echo "Version information: "
@@ -52,38 +42,6 @@ echo " - Previous version was ${PREVIOUS_VERSION}"
 echo " - There was ${MAJOR_CHANGES} major, ${MINOR_CHANGES} minor and ${PATCH_CHANGES} patch changes since then"
 echo " - Next version will be ${NEXT_VERSION}"
 
-if [[ "${REQUIRE_VERSION_TAGS}" == "true" && "${MAJOR_CHANGES}" == "0" && "${MINOR_CHANGES}" == "0" && "${PATCH_CHANGES}" == "0" ]]; then
-  echo "[ERROR] No version changes was detected (no patch, minor or major changes)."
-  export VERSION_ERROR="[ERROR] No version changes was detected (no patch, minor or major changes)."
-fi;
-
-# Do not allow to build new versions in master when release is in maintenance mode
-if [[ "${CURRENT_BRANCH}" =~ ^v[0-9]*\.[0-9]*$ ]]; then
-  MAINTENANCE_BRANCH="v${parts[0]}.${parts[1]}"
-else
-  MAINTENANCE_BRANCH="v${NEXT_MAJOR_VERSION}.${NEXT_MINOR_VERSION}"
-fi;
-
-git fetch origin "${MAINTENANCE_BRANCH}" &> /dev/null
-if [[ "$?" == "0" ]]; then
-  BUILD_REQUIRES_MAINTENANCE="1"
-else
-  BUILD_REQUIRES_MAINTENANCE=$(git branch --list | grep "${MAINTENANCE_BRANCH}" | wc -l)
-  BUILD_REQUIRES_MAINTENANCE=$(expr $BUILD_REQUIRES_MAINTENANCE + 0)
-fi;
-
-if [[ "${BUILD_REQUIRES_MAINTENANCE}" == "1" ]]; then
-  echo " - This build changes version that is in maintenance mode"
-
-  if [[ "${TRAVIS_BRANCH}" == "${MAINTENANCE_BRANCH}" && ("${MAJOR_CHANGES}" != "0" || "${MINOR_CHANGES}" != "0") ]]; then
-    echo
-    echo "[ERROR] You can not add minors or breaking changes to the version that is in maintenance mode."
-    export VERSION_ERROR="[ERROR] You can not add minors or breaking changes to the version that is in maintenance mode."
-  fi;
-fi;
-
 export PREVIOUS_VERSION=$PREVIOUS_VERSION
 export NEXT_VERSION=$NEXT_VERSION
-export BUILD_REQUIRES_MAINTENANCE=$BUILD_REQUIRES_MAINTENANCE
-export MAINTENANCE_BRANCH=$MAINTENANCE_BRANCH
 export PROJECT_VERSION=$NEXT_VERSION
